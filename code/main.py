e00cd2edeb24144c388badacca0dfef46937bc93
CookSand <funnyblackq@gmail.com>
2019-09-18 12:28:32 -0400

    update

--- code/main.py
+++ code/main.py
@@ -1,5 +1,43 @@
 import pandas as pd
 import os
-import numpy
+import numpy as np
 
-test
+! ls  
+! pwd 
+
+
+## Convert multiple files into one file and create the outputs
+
+import os
+import glob
+import pandas as pd
+
+os.chdir('./data/multiple_files')
+extension = 'csv'
+all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+
+print(all_filenames)
+
+#combine all files in the list
+combined_csv = pd.concat([pd.read_csv(f, encoding = "ISO-8859-1") for f in all_filenames ])
+#export to csv
+combined_csv.to_csv( "combined_csv.csv", index=False, encoding='utf-8-sig')
+
+
+
+
+### Create the dataframe 
+
+s = pd.Series([1, 3, 5, np.nan, 6, 8])
+dates = pd.date_range('20130101', periods=6)
+df = pd.DataFrame(np.random.randn(6, 4), index=dates, columns=list('ABCD'))
+
+
+
+df2 = pd.DataFrame({'A': 1.,
+                    'B': pd.Timestamp('20130102'),
+                    'C': pd.Series(1, index=list(range(4)), dtype='float32'),
+                    'D': np.array([3] * 4, dtype='int32'),
+                    'E': pd.Categorical(["test", "train", "test", "train"]),
+                    'F': 'foo'})
+df2.dtypes
